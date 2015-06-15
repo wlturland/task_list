a918ded57d1242e7bf2781a08cf60abc572836c6
Leigh Turland <wlturland@gmail.com>
2015-06-15 09:44:17 -0500

    Added unit testing for due date and description

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -10,7 +10,20 @@ class UserTasksController < ApplicationController
   # POST /user_tasks
   # POST /user_tasks.json
   def create
-    @user_task = UserTask.create(user_task_params)
+    @user_task = UserTask.new(user_task_params)
+
+   respond_to do |format|
+     if @user_task.save
+       format.html { redirect_to @user_task }
+       format.js {}
+       format.json { render :show, status: :created, user_task: @user_task }
+     else
+       format.html { render :new }
+       format.js { render :action => "new"}
+       format.json { render json: @user_task.errors, status: :unprocessable_entity }
+     end
+   end
+
   end
 
   # PATCH/PUT /user_tasks/1
@@ -18,14 +31,25 @@ class UserTasksController < ApplicationController
   def update
     respond_to do |format|
       if @user_task.update(user_task_params)
-        format.html { redirect_to @user_task, notice: 'User task was successfully updated.' }
+        format.html { redirect_to @user_task }
+        format.js {}
         format.json { render :show, status: :ok, location: @user_task }
       else
         format.html { render :edit }
+        format.js { render :action => "edit"}
         format.json { render json: @user_task.errors, status: :unprocessable_entity }
       end
     end
   end
+
+  def destroy
+   @user_task.destroy
+   respond_to do |format|
+     format.html { redirect_to user_tasks_url }
+     format.js {}
+     format.json { head :no_content }
+   end
+ end
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- test/models/user_task_test.rb
+++ test/models/user_task_test.rb
@@ -4,4 +4,17 @@ class UserTaskTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save without description" do
+  	user_task = UserTask.new
+  	user_task.due = Date.new(2015, 10, 31)
+   	assert_not user_task.save, "Saved task without description"
+  end
+
+  test "should not save without due date" do
+  	user_task = UserTask.new
+  	user_task.description = "Walk dog"
+  	assert_not user_task.save, "Saved task without due date"
+  end
+
 end
